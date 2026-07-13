e28673034e0fe3682af0fa2f68233e0b170cb319
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-13 17:39:36 -0500

    add regex pattern for pack version

--- pack.py
+++ pack.py
@@ -12,6 +12,11 @@ from loguru import logger
 
 __version__ = 'bcrmc-26.3snap3'
 
+VERSION_REGEX: re.Pattern[str] = re.compile(r'^bcrmc-(?P<mcversion>[0-9a-z.]+)(?:-r(?P<revision>\d+))?$')
+
+if not VERSION_REGEX.match(__version__):
+    raise ValueError(f'__version__ does not match defined pattern: {__version__!r}')
+
 logger.remove()
 
 logger.add(
