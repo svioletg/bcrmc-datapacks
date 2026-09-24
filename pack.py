84432597053c25a6a823223a22dfe38fc21fa797
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-09-24 15:06:40 -0500

    chore: bump pack.py __version__ to bcrmc-26.3

--- pack.py
+++ pack.py
@@ -10,7 +10,7 @@ from typing import Any
 from beet import Advancement, Context, Function, LootTable, Recipe
 from loguru import logger
 
-__version__ = 'bcrmc-26.3snap3'
+__version__ = 'bcrmc-26.3'
 
 VERSION_REGEX: re.Pattern[str] = re.compile(r'^bcrmc-(?P<mcversion>[0-9a-z.]+)(?:-r(?P<revision>\d+))?$')
 
